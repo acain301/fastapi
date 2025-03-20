8fdec343e45db68f88d674085ebe701be19d3b36
Leroy Kang <acain589@gmail.com>
2025-03-19 20:17:44 -0400

    Installs alembic and connection for db

--- backend/db/session.py
+++ backend/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
+print("Database URL is ",SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
