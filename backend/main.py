47f7293955c00daf7286d263aaf22d7f0f6a1dc2
Leroy Kang <acain589@gmail.com>
2025-03-22 02:53:57 -0400

    Creates post request to create blog

--- backend/main.py
+++ backend/main.py
@@ -6,16 +6,15 @@ from apis.base import api_router
 
 def create_tables():         
 	Base.metadata.create_all(bind=engine)
-        
+
+def include_router(app):
+     app.include_router(api_router)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
     create_tables()
     include_router(app)
     return app
-
-def include_router(app):
-     app.include_router(api_router)
 
 app = start_application()
 
---
+++ backend/schemas/blog.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel, model_validator
+from datetime import date
+
+
+
+class CreateBlog(BaseModel):
+    title: str 
+    slug: str 
+    content: Optional[str] = None 
+    
+    @model_validator(mode='before')
+    def generate_slug(cls, values):
+        if 'title' in values:
+            values['slug'] = values.get("title").replace(" ","-").lower()
+        return values
+
+        
+class ShowBlog(BaseModel):
+    title:str 
+    content: Optional[str]
+    created_at: date
+
+    class Config():
+        orm_mode = True
