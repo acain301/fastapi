8a8c27493bca4e75296b9cff502dd4d4fec610d6
Leroy Kang <acain589@gmail.com>
2025-03-22 03:44:51 -0400

    Adds individual blog get request query

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, status
-from sqlalchemy.orm import Session
-from fastapi import Depends
+from fastapi import(
+    APIRouter, 
+    status,
+    Depends,
+    HTTPException
+)
 
+from sqlalchemy.orm import Session
 from schemas.blog import CreateBlog, ShowBlog
 from db.session import get_db
-from db.repository.blog import create_new_blog
+from db.repository.blog import create_new_blog, retreive_blog
 
 router = APIRouter()
 
@@ -13,3 +17,10 @@ router = APIRouter()
 def create_blog(blog : CreateBlog,db: Session = Depends(get_db)):
     blog = create_new_blog(blog=blog,db=db, author_id=1)
     return blog 
+
+@router.get("/blog/{id}", response_model=ShowBlog)
+def get_blog(id: int, db: Session=Depends(get_db)):
+    blog = retreive_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(detail=f"Blog with ID {id} does not exist.", status_code=status.HTTP_404_NOT_FOUND)
+    return blog
